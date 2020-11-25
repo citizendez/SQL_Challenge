2ca9ac61a4bbdc147d23e47fb8022a178368a1d6
Desiree Herschberger <citizendez@gmail.com>
2020-11-25 17:21:12 -0500

    update bonus

--- Part-2-Case-Assignment/Data Analysis.sql
+++ Part-2-Case-Assignment/Data Analysis.sql
@@ -153,9 +153,15 @@ SELECT * FROM salary_title;
 
 
 /*Epilogue
-Evidence in hand, you march into your boss's office and present the visualization. With a sly grin, your boss thanks you for your work. On your way out of the office, you hear the words, "Search your ID number." You look down at your badge to see that your employee ID number is 499942.
+Evidence in hand, you march into your boss's office and present the visualization. 
+With a sly grin, your boss thanks you for your work. On your way out of the office, 
+you hear the words, "Search your ID number." You look down at your badge to see that your 
+employee ID number is 499942.*/
+SELECT *
+FROM employees
+WHERE emp_no = 499942;
 
-Submission
+/*Submission
 Create an image file of your ERD.
 
 Create a .sql file of your table schemata.
