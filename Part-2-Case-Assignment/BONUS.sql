f1abad77274c8036241209c1024894e5927c4640
Desiree Herschberger <citizendez@gmail.com>
2020-11-26 10:49:03 -0500

    clean up data

--- Part-2-Case-Assignment/BONUS.sql
+++ Part-2-Case-Assignment/BONUS.sql
@@ -1,3 +1,8 @@
 /*Bonus (Optional)
 
 Create a histogram to visualize the most common salary ranges for employees.*/
+DROP VIEW IF EXISTS employee_salaries;
+CREATE VIEW employee_salaries AS
+SELECT
+	salary
+FROM salaries;
