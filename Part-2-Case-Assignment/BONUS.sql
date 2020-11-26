414ebc51a43c81d6beda7ed3600d59345cb8394c
Desiree Herschberger <citizendez@gmail.com>
2020-11-26 10:50:18 -0500

    update bonus file

--- Part-2-Case-Assignment/BONUS.sql
+++ Part-2-Case-Assignment/BONUS.sql
@@ -6,3 +6,20 @@ CREATE VIEW employee_salaries AS
 SELECT
 	salary
 FROM salaries;
+
+-- SELECT salary FROM salaries;
+--Create a bar chart of average salary by title.
+DROP VIEW IF EXISTS salary_title;
+CREATE VIEW salary_title AS
+SELECT
+	t.title,
+	AVG(s.salary) as Avg_Salary
+FROM 
+	employees e
+	INNER JOIN titles t
+	ON e.emp_title_id = t.title_id
+	INNER JOIN salaries s
+	ON s.emp_no = e.emp_no
+GROUP BY t.title;
+
+SELECT * FROM salary_title;
